7e02552d5ed16191de2c5510d3d9a33bb49366cd
Aibek Sadraliev <a.sadraliev@gmail.com>
2025-04-22 21:17:18 -0400

    fix: change env name for endpoint

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ npm install -g npm@11.2.0 || exit
 
 read -p "Please enter your backend ip address: " backend
 
-echo "BACKEND_URL=http://$backend:8000" > .env.local
+echo "NEXT_PUBLIC_BACKEND_URL=http://$backend:8000" > .env.local
 
 echo "✅ Node environment is ready. Build your app by exeucuting:"
 echo "npm run build"
